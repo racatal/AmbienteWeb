eb55d6fe00fe6968f79b7ef2b19f9b1f30f8d4e5
FlavMora <franciscomh98@gmail.com>
2025-07-27 02:47:28 -0600

    update

--- layout/footer.php
+++
@@ -1,7 +0,0 @@
-
-
-<footer class="footer py-3 text-center">
-    <div class="container">
-      <small>&copy; <?= date('Y') ?> Pet Homeless</small>
-    </div>
-  </footer>
--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset();     // Elimina todas las variables de sesión
-session_destroy();   // Destruye la sesión
-
-header('Location: index.php'); 
-exit;
